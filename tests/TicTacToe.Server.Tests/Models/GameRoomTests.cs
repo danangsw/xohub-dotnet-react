41311d01d0db1436c56ec2ddbefb7b087a9412b3
danang sukma wantara <danangsukma@gmail.com>
2025-09-29 23:04:39 +0700

    Add Unit Test GameRoom Model

---
+++ tests/TicTacToe.Server.Tests/Models/GameRoomTests.cs
@@ -0,0 +1,201 @@
+// server/TicTacToe.Server.Tests/Models/GameRoomTests.cs
+using FluentAssertions;
+using XoHub.Server.Models;
+using XoHub.Server.Services;
+using Xunit;
+
+namespace XoHub.Server.Tests.Models;
+
+public class GameRoomTests
+{
+    [Fact]
+    public void Constructor_ShouldInitializeWithDefaultValues()
+    {
+        // Arrange & Act
+        var room = new GameRoom();
+
+        // Assert
+        room.RoomId.Should().BeEmpty();
+        room.Players.Should().HaveCount(2);
+        room.Players[0].Should().BeNull();
+        room.Players[1].Should().BeNull();
+        room.Players.Should().AllSatisfy(player => player.Should().BeNull());
+        room.Board.Should().BeOfType<char[,]>();
+        room.Board.GetLength(0).Should().Be(3); // rows
+        room.Board.GetLength(1).Should().Be(3); // columns
+        room.CurrentTurn.Should().Be("X");
+        room.Status.Should().Be(GameStatus.WaitingForPlayers);
+        room.IsAIMode.Should().BeFalse();
+        room.Winner.Should().BeNull();
+        room.MoveCount.Should().Be(0);
+        room.AIDifficulty.Should().Be(DifficultyLevel.Medium);
+    }
+
+    [Fact]
+    public void IsGameActive_ShouldReturnTrue_WhenStatusIsInProgress()
+    {
+        // Arrange
+        var room = new GameRoom { Status = GameStatus.InProgress };
+
+        // Act & Assert
+        room.IsGameActive.Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsDraw_ShouldReturnTrue_WhenFinishedAndNoWinner()
+    {
+        // Arrange
+        var room = new GameRoom
+        {
+            Status = GameStatus.Finished,
+            Winner = null
+        };
+
+        // Act & Assert
+        room.IsDraw.Should().BeTrue();
+    }
+
+    [Fact]
+    public void GetCurrentPlayer_ShouldReturnPlayerWithCurrentTurnMark()
+    {
+        // Arrange
+        var room = new GameRoom { CurrentTurn = "X" };
+        var playerX = new Player { Mark = 'X', Name = "Alice" };
+        var playerO = new Player { Mark = 'O', Name = "Bob" };
+        room.Players[0] = playerX;
+        room.Players[1] = playerO;
+
+        // Act
+        var currentPlayer = room.GetCurrentPlayer();
+
+        // Assert
+        currentPlayer.Should().Be(playerX);
+    }
+
+    [Fact]
+    public void IsValidMove_ShouldReturnTrue_ForEmptyCellWithinBounds()
+    {
+        // Arrange
+        var room = new GameRoom();
+        room.Board[1, 1] = '\0'; // Empty cell
+
+        // Act & Assert
+        room.IsValidMove(1, 1).Should().BeTrue();
+    }
+
+    [Fact]
+    public void IsValidMove_ShouldReturnFalse_ForOccupiedCell()
+    {
+        // Arrange
+        var room = new GameRoom();
+        room.Board[1, 1] = 'X'; // Occupied cell
+
+        // Act & Assert
+        room.IsValidMove(1, 1).Should().BeFalse();
+    }
+
+    [Fact]
+    public void IsValidMove_ShouldReturnFalse_ForOutOfBounds()
+    {
+        // Arrange
+        var room = new GameRoom();
+
+        // Act & Assert
+        room.IsValidMove(-1, 0).Should().BeFalse();
+        room.IsValidMove(0, -1).Should().BeFalse();
+        room.IsValidMove(3, 0).Should().BeFalse();
+        room.IsValidMove(0, 3).Should().BeFalse();
+    }
+
+    [Fact]
+    public void GetAvailableMoves_ShouldReturnAllEmptyCells()
+    {
+        // Arrange
+        var room = new GameRoom();
+        room.Board[0, 0] = 'X';
+        room.Board[1, 1] = 'O';
+        // Other cells remain empty
+
+        // Act
+        var moves = room.GetAvailableMoves();
+
+        // Assert
+        moves.Should().HaveCount(7); // 9 total - 2 occupied
+        moves.Should().Contain((0, 1));
+        moves.Should().Contain((0, 2));
+        moves.Should().Contain((1, 0));
+        moves.Should().Contain((1, 2));
+        moves.Should().Contain((2, 0));
+        moves.Should().Contain((2, 1));
+        moves.Should().Contain((2, 2));
+    }
+
+    [Fact]
+    public void CloneBoard_ShouldCreateIndependentCopy()
+    {
+        // Arrange
+        var room = new GameRoom();
+        room.Board[0, 0] = 'X';
+        room.Board[1, 1] = 'O';
+
+        // Act
+        var clone = room.CloneBoard();
+
+        // Assert
+        clone[0, 0].Should().Be('X');
+        clone[1, 1].Should().Be('O');
+        clone[2, 2].Should().Be('\0');
+
+        // Modify original - clone should be unaffected
+        room.Board[2, 2] = 'X';
+        clone[2, 2].Should().Be('\0');
+    }
+
+    [Fact]
+    public void SerializeBoardState_ShouldReturnCorrectStringRepresentation()
+    {
+        // Arrange
+        var room = new GameRoom();
+        room.Board[0, 0] = 'X';
+        room.Board[0, 2] = 'O';
+        room.Board[2, 2] = 'X';
+
+        // Act
+        var serialized = room.SerializeBoardState();
+
+        // Assert
+        // Current SerializeBoardState produces 17 characters:
+        // Board layout: X _ O
+        //               _ _ _  
+        //               _ _ X
+        // Serialized as: "X   O           X" (with spaces after each cell except (2,2))
+        serialized.Should().Be("X   O           X");
+    }
+
+    [Theory]
+    [InlineData(GameStatus.WaitingForPlayers, 0)]
+    [InlineData(GameStatus.InProgress, 100)] // Some duration
+    [InlineData(GameStatus.Finished, 200)]   // Final duration
+    public void GameDuration_ShouldCalculateCorrectly(GameStatus status, int expectedSeconds)
+    {
+        // Arrange
+        var room = new GameRoom
+        {
+            Status = status,
+            CreatedAtUtc = DateTime.UtcNow.AddSeconds(-expectedSeconds)
+        };
+
+        // Act
+        var duration = room.GameDuration;
+
+        // Assert
+        if (status == GameStatus.Finished)
+        {
+            duration.TotalSeconds.Should().BeApproximately(expectedSeconds, 1);
+        }
+        else
+        {
+            duration.Should().Be(TimeSpan.Zero);
+        }
+    }
+}
